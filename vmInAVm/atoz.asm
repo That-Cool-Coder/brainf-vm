811169cd8045125839007573d44194426a9fe85d
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2023-01-06 18:23:20 +1030

    Add make atoz 16x faster

--- vmInAVm/atoz.asm
+++ vmInAVm/atoz.asm
@@ -2,11 +2,14 @@
 
 ; r2 is current character value
     set         2   65
+; r3 is end of loop
+    set         3   91
 
 label       start_loop
     move        2   0
     putchar
     increment
     copy        0   2
-    shrink      91 ; 90 is value of Z, we do 91 to avoid OBOE
+    copy        3   1
+    sub
     jumpif      start_loop
